35f31839d3873b7a673bd70fec8b1a46e7f08415
A-Hilaly <hilalyamine@gmail.com>
2017-08-04 11:12:53 +0200

    Fix query generators

--- zilean/mysql_utils/mysql_query.py
+++ zilean/mysql_utils/mysql_query.py
@@ -1,4 +1,7 @@
 
+def _get_protocols():
+    pass
+
 # Databases querries
 _SHOW_DATABASES = "SHOW DATABASES;"
 _CREATE_DATABASE = "CREATE DATABASE {0};"
@@ -8,12 +11,44 @@ _USE_DATABASE = "USE {0};"
 # Table querries
 _SHOW_TABLE_FIELDS = "DESC {0}.{1};"
 _SHOW_ALL_TABLES = "SHOW TABLES IN {0};"
+
 _CREATE_TABLE = """
 CREATE TABLE {0}.{1} (
-    {2}
+  {2}
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 """
+
+# /!\ Warning in the next 2 functions :
+# Using db or table as kwargs will
+# cause some obvious conficlts
+def _dp(s, mode=None):
+    # decode dict protocol
+    pass
+
+def _make_field_line2(a, b, comma=True, new_line=True):
+    if comma and new_line: return "  `{0}` {1},\n"
+    elif comma and not new_line: return"  `{0}` {1},"
+    elif not comma and new_line: return"  `{0}` {1}\n"
+    else: return"  `{0}` {1}"
+
+def _make_extra_line2():
+    pass
+
+def _p_cqt(db=None, table=None, **kwargs):
+    # protocol call
+    nkwargs = _dp(kwargs, mode="safe")
+    n = len(nkwargs)
+    for field, _type in nkwargs.items():
+        pass
+
+
+def _ha_cqt(db=None, table=None, **kwargs):
+    # half assisted
+    pass
+
 _DELETE_TABLE = "DROP TABLE {0};"
+
+#COLUMN
 _ADD_COLUMN = """
     ALTER TABLE {0}
     ADD {1} {2};"""
