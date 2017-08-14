47542e931ed07121dec2169fbf21fbfb62bb39a5
A.H <hilalyamine@gmail.com>
2017-08-14 18:53:59 +0200

    Update zilean.py

--- zilean/zilean.py
+++ zilean/zilean.py
@@ -1,10 +1,8 @@
 def main():
     pass
 
-
 def server_access_autorisation(**kwargs):
     pass
-
 
 def make_server_partition(**kwargs):
     pass
@@ -15,14 +13,11 @@ def add_server_partition(**kwargs):
 def remove_server_partition(**kwargs):
     pass
 
-
 def backup_server_partition(**kwargs):
     pass
 
-
 def reconstruct_server_partition(**kwargs):
     pass
-
 
 if __name__ == "__main__":
     main()
