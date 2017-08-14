bcc29a741000f6da08c2e8ea252141f3db8d16c9
A-Hilaly <hilalyamine@gmail.com>
2017-08-14 12:21:52 +0200

    Zilean path

--- make-zilean.sh
+++ make-zilean.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 ZILEAN_PATH="$HOME/zilean"
-ZILEAN_SQL="$ZILEAN_PATH/zilean/zilean/_sql"
+ZILEAN_SQL="$ZILEAN_PATH/zilean/_sql"
 ZILEAN_WORKING_DIR="$HOME/work/zilean"
 #XXX: the next line should be already exported at the machine
 # creation
 #MACHINE_CONFIG = "$HOME/zilean-machine.ini"
-
 
 function expmk () {
   export ZILEAN_PATH
