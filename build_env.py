e03a2b3f67e89fa3b9a4fb05e61b777b76a9efdd
A.H <hilalyamine@gmail.com>
2017-09-22 17:17:04 +0200

    Add

--- build_env.py
+++ build_env.py
@@ -1,19 +1,20 @@
 import os
 import skmvs as SK
 
-#TODO: REDO
-ZP = os.getcwd()
+ZP = os.environ['ZILEAN_PATH']
 ZS = "{0}/scripts".format(ZP)
-ZV = "0.0.1"
+ZAPI = "{0}/api".formar(ZP)
+ZV = os.environ['ZILEAN_VERSION']
 
-def _record_var_envs():
-    SK.store_value("ZILEAN_PATH", ZP)
-    SK.get_value("ZILEAN_SCRIPTS", ZS)
-    ZILEAN_VERSION = SK.get_value("ZILEAN_VERSION", ZV)
+def build():
+    SK.store_value("ZILEAN_PATH", ZP, db='paths', force=True)
+    SK.store_value("ZILEAN_SCRIPTS", ZS, db='paths', force=True)
+    SK.store_value("ZILEAN_API", ZAPI, db='paths', force=True)
+    SK.store_value("ZILEAN_VERSION", ZV, db='main', force=True)
+    SK.store_value("zilean_paths_stored", True, db='main', force=True)
 
 def make():
-    _record_var_envs()
-
+    build()
 
 if __name__ == "__main__":
     make()
